674f10a71e1d69323bcbd4d2d6980e8fe9e10032
kalpeshchhowala <kalpesh.chhowala@gmail.com>
2024-07-19 08:42:59 +0530

    Required Footer redesign done by Kalpesh

--- Tollance University Web React Sample/src/components/common/Footer.js
+++ Tollance University Web React Sample/src/components/common/Footer.js
@@ -6,49 +6,27 @@ export const Footer = () => {
     <div>
       <footer className="tl-footer tl-4-footer">
         <div className="tl-footer-top">
-          <div className="container">
-            <div className="row gy-5 justify-content-between">
-              <div className="col-lg-3 col-sm-6 col-12">
-                <div className="tl-footer-widget tl-4-footer-widget">
-                  <Link to="/" className="logo tl-footer-widget-title">
-                    <img src="assets/images/logos/logo-3.png" alt="logo" />
-                  </Link>
-
-                  <p className="tl-4-footer-descr">
-                    At Tollence University, we are committed to fostering a
-                    culture of excellence in education, research, and
-                    innovation.
+          <div className="container">           
+            {/* New Required Footer start here */}
+             <div className="row gy-5 justify-content-between align-items-center">
+              <div className="col-lg-4 order-lg-1 order-3">
+                <div className="tl-footer-widget tl-4-footer-widget">                  
+                  <p className="tl-4-copyright-txt m-0 text-center text-lg-left">
+                    ©2024 Tollense University 
                   </p>
-                  <div className="tl-4-footer-socials">
-                    <ul>
-                      <li>
-                        <a href="https://twitter.com/" target="_blank">
-                          <i className="fa-brands fa-twitter"></i>
-                        </a>
-                      </li>
-                      <li>
-                        <a href="https://www.facebook.com/" target="_blank">
-                          <i className="fa-brands fa-facebook-f"></i>
-                        </a>
-                      </li>
-                      <li>
-                        <a href="https://www.linkedin.com/" target="_blank">
-                          <i className="fa-brands fa-linkedin-in"></i>
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
                 </div>
               </div>
 
-              <div className="col-lg-4 order-1 order-sm-2 order-lg-1">
-                <div className="row gy-5">
-                  <div className="col-6 col-xxs-12">
-                    <div className="tl-footer-widget tl-4-footer-widget">
-                      <h5 className="tl-footer-widget-title tl-4-footer-widget-title">
-                        Our Campus
-                      </h5>
-                      <ul className="tl-footer-links tl-4-footer-links">
+              <div className="col-lg-4 order-1 order-lg-2">
+                    <div className="tl-footer-widget tl-4-footer-widget text-center">
+                      <Link to="/" className="logo tl-footer-widget-title mb-0">
+                      <img src="assets/images/logos/logo-3-light.png" alt="logo" />
+                      </Link>
+                  </div>                  
+                </div>
+                <div className="col-lg-4 order-2 order-lg-3">
+                <div className="tl-footer-widget tl-4-footer-widget">                  
+                <ul className="tl-footer-links tl-4-footer-links flex-row justify-content-center">
                         <li>
                           <Link to="/" role="button">
                             Home
@@ -73,86 +51,13 @@ export const Footer = () => {
                           </Link>
                         </li>
                       </ul>
-                    </div>
-                  </div>
-                  <div className="col-6 col-xxs-12">
-                    <div className="tl-footer-widget tl-4-footer-widget">
-                      <h5 className="tl-footer-widget-title tl-4-footer-widget-title">
-                        Contact
-                      </h5>
-                      <ul className="tl-footer-links tl-4-footer-links">
-                        <li>
-                          <a href="tel:12356877787">+49 6221 54-0</a>
-                        </li>
-                        <li>
-                          <p>
-                            Tollense University Grabengasse 1 69117 Heidelberg
-                          </p>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
+                 
                 </div>
               </div>
-
-              <div className="col-lg-4 col-sm-6 col-12 order-2 order-sm-1 order-lg-2">
-                <div className="tl-footer-widget tl-4-footer-widget">
-                  <h5 className="tl-footer-widget-title tl-4-footer-widget-title">
-                    Recent Posts
-                  </h5>
-
-                  <div className="tl-4-footer-articles">
-                    <div className="tl-4-footer-article">
-                      <img
-                        src="assets/images/tl-11/sidebar-article-2.jpg"
-                        alt="Article image"
-                        className="tl-4-footer-article-img"
-                      />
-
-                      <div className="tl-4-footer-article-txt">
-                        <span className="tl-4-footer-article-date">
-                          June 16, 2023
-                        </span>
-                        <h5 className="tl-4-footer-article-title">
-                          Education Renaissance Reviving the Art of Learning.
-                        </h5>
-                      </div>
-                    </div>
-
-                    <div className="tl-4-footer-article">
-                      <img
-                        src="assets/images/tl-11/sidebar-article-3.jpg"
-                        alt="Article image"
-                        className="tl-4-footer-article-img"
-                      />
-
-                      <div className="tl-4-footer-article-txt">
-                        <span className="tl-4-footer-article-date">
-                          April 18, 2023
-                        </span>
-                        <h5 className="tl-4-footer-article-title">
-                          Education Trailblazers Innovating the Learning.
-                        </h5>
-                      </div>
-                    </div>
-                  </div>
-                </div>
               </div>
+              {/* New Required Footer end here */}
             </div>
           </div>
-        </div>
-
-        <div className="tl-footer-bottom tl-4-footer-bottom">
-          <div className="container">
-            <div className="row gy-4 align-items-center">
-              <div className="col-12">
-                <p className="tl-4-copyright-txt m-0 text-center">
-                  ©2024 Tollense University
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
       </footer>
     </div>
   )
